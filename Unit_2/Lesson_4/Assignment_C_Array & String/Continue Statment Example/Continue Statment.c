ab33e97783449dc5d4fc49ec355d7725d5b25cf3
Eng.Abdelaziz Alsayed <Todoroki_Shouto_14@gmail.com>
2024-08-16 23:45:21 +0300

    Solution of Example Break Statment

---
+++ Unit_2/Lesson_4/Assignment_C_Array & String/Continue Statment Example/Continue Statment.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    int Max_Num , Count = 0 ;
+    float Num , Product = 1.0 ;
+ 
+
+    // Loop to get user inputs ( 4 Only)
+      for ( int i = 1 ; i <= 4 ; i++) 
+    {
+      printf ("\n Enter Value of Number \" %d \" : " , i );
+      scanf ("%f",&Num);
+      if ( Num == 0) 
+      {
+        continue;
+      }
+      Product *= Num;
+      Count ++ ;
+    }
+        // Calculate the Product
+      printf ("\n The Product of %d Input Numbers Correct is : %0.2f \n " , Count , Product );
+    
+
+    // Ensure output is visible and wait for user input
+    printf("\n Program finished. Press Enter any Key to exit...\n");
+    getchar(); // Consume the newline character left by the last scanf
+    getchar(); // Wait for user input before closing
+}
